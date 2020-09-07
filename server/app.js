40d3c300d1f6c2aec8e838646370ea3495af6b76
sy-permana <suryapermana996@gmail.com>
2020-09-08 00:39:24 +0800

    change node_env

--- server/app.js
+++ server/app.js
@@ -1,4 +1,7 @@
-require('dotenv').config()
+if(process.env.NODE_ENV === "development"){
+    require('dotenv').config();
+}
+
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT;
